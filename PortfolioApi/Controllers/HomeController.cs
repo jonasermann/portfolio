878c3e78258b4c42303eb7923bbfe9d63b882221
Jonas Ermann <jonas.ermann@hotmail.com>
2022-09-01 11:15:01 +0200

    Add web Api

---
+++ PortfolioApi/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HomeController : ControllerBase
+{
+
+}
---
+++ PortfolioApiTests/UnitTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using FluentAssertions;
+
+namespace PortfolioApiTests;
+
+public class UnitTest
+{
+    [Fact]
+    public void Test()
+    {
+
+    }
+}
