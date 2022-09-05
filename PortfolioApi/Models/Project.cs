48e9d98d91f69ed2d2a0635b9852d3d7e262452e
Jonas Ermann <jonas.ermann@hotmail.com>
2022-09-05 12:17:27 +0200

    Add Project Model

---
+++ PortfolioApi/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace PortfolioApi.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public string Url { get; set; }
+    }
+}
