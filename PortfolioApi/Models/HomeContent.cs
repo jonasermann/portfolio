b593e422f7354e7980b9550df0524025a14ad174
Jonas Ermann <jonas.ermann@hotmail.com>
2022-09-05 15:00:25 +0200

    Add HomeContent Model

---
+++ PortfolioApi/Models/HomeContent.cs
@@ -0,0 +1,11 @@
+﻿namespace PortfolioApi.Models
+{
+    public class HomeContent
+    {
+        public int Id { get; set; }
+
+        public string ProfilePicUrl { get; set; }
+
+        public string Text { get; set; }
+    }
+}
