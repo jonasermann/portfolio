9436a4f8102bef50aaa832184b40508e00d7db88
Jonas Ermann <jonas.ermann@hotmail.com>
2022-09-05 12:49:13 +0200

    Add Projects Controller

--- PortfolioApi/Models/Project.cs
+++ PortfolioApi/Models/Project.cs
@@ -1,15 +1,14 @@
-﻿namespace PortfolioApi.Models
+﻿namespace PortfolioApi.Models;
+
+public class Project
 {
-    public class Project
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public string Title { get; set; }
+    public string Title { get; set; }
 
-        public string ImgUrl { get; set; }
+    public string ImgUrl { get; set; }
 
-        public string Text { get; set; }
+    public string Text { get; set; }
 
-        public string GitUrl { get; set; }
-    }
+    public string GitUrl { get; set; }
 }
--- PortfolioApi/Models/ProjectDTO.cs
+++ PortfolioApi/Models/ProjectDTO.cs
@@ -1,13 +1,12 @@
-﻿namespace PortfolioApi.Models
+﻿namespace PortfolioApi.Models;
+
+public class ProjectDTO
 {
-    public class ProjectDTO
-    {
-        public string Title { get; set; }
+    public string Title { get; set; }
 
-        public string ImgUrl { get; set; }
+    public string ImgUrl { get; set; }
 
-        public string Text { get; set; }
+    public string Text { get; set; }
 
-        public string GitUrl { get; set; }
-    }
+    public string GitUrl { get; set; }
 }
