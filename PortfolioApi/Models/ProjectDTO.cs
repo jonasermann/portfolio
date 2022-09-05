04ce875c29f6ab7bef7be05226930c871c05b006
Jonas Ermann <jonas.ermann@hotmail.com>
2022-09-05 12:23:13 +0200

    Rafactor ProjectDTO

--- PortfolioApi/Models/ProjectDTO.cs
+++ PortfolioApi/Models/ProjectDTO.cs
@@ -4,8 +4,10 @@
     {
         public string Title { get; set; }
 
+        public string ImgUrl { get; set; }
+
         public string Text { get; set; }
 
-        public string Url { get; set; }
+        public string GitUrl { get; set; }
     }
 }
