905bdf496781eb44b06a6836297773f553157bb3
jonasermann <103424073+jonasermann@users.noreply.github.com>
2022-10-13 19:51:51 +0200

    Refactor

--- PortfolioApi/Controllers/AuthenticationController.cs
+++ PortfolioApi/Controllers/AuthenticationController.cs
@@ -17,7 +17,7 @@ public class AuthenticationController : ControllerBase
     }
 
     [HttpPost]
-    public IActionResult Post([FromBody] string password)
+    public IActionResult Post(string password)
     {
         var token = _jWTAuthenticationManager.Authenticate(password);
 
